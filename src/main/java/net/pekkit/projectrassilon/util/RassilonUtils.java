fe0978105046e2e3bda38a7bc4d3a81d01bd8588
eccentricdevotion <eccentricdevotion@gmail.com>
2016-04-17 22:26:28 +1200

    Cursory update for Bukkit 1.9

--- src/main/java/net/pekkit/projectrassilon/util/RassilonUtils.java
+++ src/main/java/net/pekkit/projectrassilon/util/RassilonUtils.java
@@ -25,15 +25,15 @@ package net.pekkit.projectrassilon.util;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
-import net.minecraft.server.v1_8_R3.PacketPlayOutTitle.EnumTitleAction;
-import net.minecraft.server.v1_8_R3.IChatBaseComponent;
-import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
-import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
-import net.minecraft.server.v1_8_R3.PlayerConnection;
+import net.minecraft.server.v1_9_R1.IChatBaseComponent;
+import net.minecraft.server.v1_9_R1.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_9_R1.PacketPlayOutChat;
+import net.minecraft.server.v1_9_R1.PacketPlayOutTitle;
+import net.minecraft.server.v1_9_R1.PacketPlayOutTitle.EnumTitleAction;
+import net.minecraft.server.v1_9_R1.PlayerConnection;
 import net.pekkit.projectrassilon.ProjectRassilon;
 import org.bukkit.ChatColor;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 /**
@@ -94,7 +94,7 @@ public class RassilonUtils {
             return index;
         }
     }
-    
+
     public static void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
         CraftPlayer craftplayer = (CraftPlayer) player;
         PlayerConnection connection = craftplayer.getHandle().playerConnection;
@@ -111,7 +111,7 @@ public class RassilonUtils {
         PacketPlayOutChat ppoc = new PacketPlayOutChat(cbc, (byte) 2);
         ((CraftPlayer) p).getHandle().playerConnection.sendPacket(ppoc);
     }
-    
+
     private static String buildJSON(String msg) {
         return "{text:\"" + ChatColor.translateAlternateColorCodes('&', msg) + "\"}";
     }
